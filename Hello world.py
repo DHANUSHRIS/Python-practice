8fd8829909662d9be5a69ce1d5541e9a1815a16c
Dhanushri s <90515404+Dhanushri-S@users.noreply.github.com>
2021-09-11 20:28:43 +0530

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,46 @@
+name= "Hello World"
+length= len(name)
+print("Single Character :",name[3])
+print("Length of string :",length)
+print("Muiltiple character :",name[1:2])
+print("Multiple character start to end : ",name[0::])
+
+lastindex= length-1
+print("last element:",lastindex)
+
+print("last single element:",name[-3])
+print("last multiple elemts:",name[:-1])
+
+
+
+# 1. Addition
+x = 60
+y = 100
+add= x+y
+print("Addition:",add)
+
+#2. substraction
+sub = x-y
+print("Subtraction:",sub)
+
+#3.multiplication
+mul = x*y
+print("Multiplication:",mul)
+
+#4. Division
+Div = x/y
+print("Divsion:", Div)
+
+#5. modulus
+Mod = x%y
+print("Modulus:",Mod)
+
+#6. division
+div = x//y
+print("Divsion with //:", div)
+
+
+
+
+
+
